d6dc11fd68a7e809afc180e83aad93b3e6af568b
Shraysi Dubey <65558817+shraysidubey@users.noreply.github.com>
2024-01-13 16:14:57 +0530

    Update queueUsingList.py
    Modified functions

--- queueUsingList.py
+++ queueUsingList.py
@@ -1,46 +1,46 @@
 #Implementation of Queue using List
 class Queue:
-    def __init__(self):
-        self.list = []
-
-    def enqueue(self,item):
-        self.list.append(item)
-        return self.list
-
-    def dequeue(self):
-        self.list.pop(0)
-        return self.list
-
-    def isEmpty(self):
-        if len(self.list) == 0:
-            return True
-        else:
-            return False
-
-    def size(self):
-        return len(self.list)
     
+    def __init__(self):
+        self.lst = []
 
-q = Queue()
-S = q.enqueue(10)
-print(S)
+    def enqueue(self, element):
+        self.lst.append(element)
+        return self.lst
+    
+    def dequeue(self):
+        self.lst.pop(0)
+        return self.lst
+    
+    def front(self):
+        return "The front value of the queue " + str(self.lst[0])
+    
+    def rear(self):
+        return "The rear value of the queue " + str(self.lst[len(self.lst)-1])
+    
+    def size_of_queue(self):
+        return "The length of the queue " + str(len(self.lst))
+    
+    def is_empty(self):
+        if len(self.lst) == 0:
+            return "The queue is empty"
+        else:
+            return "The is not empty" + str(self.lst)
+        
+    def __str__(self) -> str:
+        print("The queue is" + str(self.lst))
 
-S = q.enqueue(20)
-print(S)
+obj_a = Queue()
+obj_a.enqueue("a")
+obj_a.enqueue("b")
+obj_a.enqueue("c")
+obj_a.enqueue("d")
+obj_a.enqueue("e")
+obj_a.enqueue("g")
+obj_a.__str__()
+print(obj_a.dequeue())
 
-S = q.enqueue(30)
-print(S)
-
-S = q.enqueue(40)
-print(S)
-
-S = q.enqueue(50)
-print(S)
-
-S = q.dequeue()
-print(S)
-S = q.isEmpty()
-print(S)
-S = q.size()
-print(S)
+print(obj_a.front())  #b
+print(obj_a.size_of_queue())
+obj_a.__str__()
 
